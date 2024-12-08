a925bbca1e95e702e12f11c2074b2ee19231a531
ayroop <pooriya@live.com>
2024-12-09 01:49:20 +0330

    Internationalize CustomLoggerServiceProvider: Wrap channel names with translation functions

--- src/Providers/CustomLoggerServiceProvider.php
+++ src/Providers/CustomLoggerServiceProvider.php
@@ -18,26 +18,39 @@ class CustomLoggerServiceProvider extends AbstractServiceProvider implements Boo
      */
     protected $plugin;
 
+    /**
+     * Constructor.
+     *
+     * @param \Rabbit\Plugin $plugin The plugin instance.
+     */
     public function __construct($plugin)
     {
         $this->plugin = $plugin;
     }
 
+    /**
+     * Register services.
+     */
     public function register()
     {
         $container = $this->getContainer();
 
         // Retrieve configuration values
-        $logsPath = $this->plugin->config('logs_path');
-        $logsDays = $this->plugin->config('logs_days');
+        $logsPath      = $this->plugin->config('logs_path');
+        $logsDays      = $this->plugin->config('logs_days');
         $enableLogging = $this->plugin->config('options.enable_logging', false);
+
+        // Determine the logging channel
+        $channel = (defined('WP_DEBUG') && WP_DEBUG)
+            ? __('development', 'book-information')
+            : __('production', 'book-information');
 
         // Always register the 'logger' service
         if ($enableLogging && $logsPath && $logsDays) {
             $container->add('logger', Logger::class)
                 ->addArgument([
                     'dir_name'  => $logsPath,
-                    'channel'   => defined('WP_DEBUG') && WP_DEBUG ? 'development' : 'production',
+                    'channel'   => $channel,
                     'logs_days' => $logsDays,
                 ]);
         } else {
@@ -46,14 +59,19 @@ class CustomLoggerServiceProvider extends AbstractServiceProvider implements Boo
         }
     }
 
+    /**
+     * Boot the plugin provider.
+     */
     public function bootPlugin()
     {
         $instance = $this;
 
+        // Add a macro 'logger' to the container
         $this->getContainer()::macro('logger', function () use ($instance) {
             return $instance->getContainer()->get('logger');
         });
 
+        // Ensure the logger is initialized during 'init' action
         add_action('init', function () use ($instance) {
             $instance->getContainer()->get('logger');
         });
